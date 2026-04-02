169acf086cebba54f146ff4e7d18259a1b1c25be
Humbs <humbsb2013@gmail.com>
2026-04-02 14:46:53 +0300

    Added the src folder

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('It works!');
+    res.send('It works!')
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running');
-});
+    console.log('Server is running')
+})
